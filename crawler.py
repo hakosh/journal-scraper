5f6e35a97b27c28bb1265e9833b2bffbf3294f37
Akos Hajas <hakosh2@gmail.com>
2023-08-21 19:50:14 -0400

    Fix error message

--- crawler.py
+++ crawler.py
@@ -57,7 +57,7 @@ class Crawler:
 
         exception = task.exception()
         if exception is not None:
-            print(f'bad happened: {exception}')
+            print(f'error happened while fetching {link.url}: {type(exception)}')
             status = "failed"
         else:
             self._enqueue(task.result())
